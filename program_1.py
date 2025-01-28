3dc76f92496b866290dfd4c39bb9ed7bc04f577b
egstatema <egstatema@students.unwsp.edu>
2025-01-28 12:44:47 -0600

    Add files via upload

--- program_1.py
+++ program_1.py
@@ -1,6 +1,13 @@
-def personal_information():
-    print('...')
+# Eliya Statema
+# 1/28/25
+# PERSONAL INFORMATION
 
+name = "Eliya Statema"
+address = "123 Main Street, Smalltown, MN, 12345"
+phone_number = "123-456-7899"
+college_major = "Graphic design"
 
-# Line which calls the above function.
-personal_information()
+print(f'''Name: {name} 
+Address: {address} 
+Phone Number: {phone_number} 
+College Major: {college_major}''')
--- program_3.py
+++ program_3.py
@@ -1,8 +1,33 @@
-def calculate_total_purchase():
-    # A customer in a store is purchasing five items.  
-    # Write a program that asks for each item, 
-    # then displays the subtotal of the sale, 
-    # the amount of sales tax, and the total.  
-    # Assume the sales tax is 7 percent.
+# Eliya Statema
+# 1/28/25
+# TOTAL PURCHASE
 
-calculate_total_purchase()
+tax_rate = 0.07
+
+print('''You are purchasing five items. Write the price of each item below. 
+Do not include $ in your answer.
+''')
+
+print("ITEM 1")
+item_1 = float(input("Price: "))
+
+print("ITEM 2")
+item_2 = float(input("Price: "))
+
+print("ITEM 3")
+item_3 = float(input("Price: "))
+
+print("ITEM 4")
+item_4 = float(input("Price: "))
+
+print("ITEM 5")
+item_5 = float(input("Price: "))
+
+subtotal = item_1 + item_2 + item_3 + item_4 + item_5
+sales_tax = subtotal * tax_rate
+total = subtotal + sales_tax
+
+print(f'''
+SUBTOTAL: ${subtotal:.2f}
+SALES TAX: ${sales_tax:.2f}
+*** TOTAL: ${total:.2f} ***''')
